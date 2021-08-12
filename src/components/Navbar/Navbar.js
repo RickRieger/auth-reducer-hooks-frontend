1c30f0f3dda7c7f2ce9160f256773b160d57fa1a
Rick Rieger <rickrieger36251@gmail.com>
2021-08-11 21:32:10 -0400

    added backend

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { NavLink, Link } from 'react-router-dom';
+
+import { AuthContext } from '../../context/AuthContext';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Navbar() {
-  return <div></div>;
+  const classes = useStyles();
+
+  const {
+    state: { user },
+  } = useContext(AuthContext);
+
+  const isUserLoggedIn = user ? true : false;
+  const navLinkTitleOne = isUserLoggedIn ? '/profile' : '/login';
+  const navLinkDisplayOne = isUserLoggedIn ? `${user.email}` : 'login';
+  const navLinkTitleTwo = isUserLoggedIn ? '/logout' : '/sign-up';
+  const navLinkDisplayTwo = isUserLoggedIn ? 'Logout' : 'Sign up';
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' className={classes.title}>
+            <Link to='/'>React Auth Hooks Fullstack</Link>
+          </Typography>
+
+          <NavLink activeStyle={{ color: 'red' }} exact to={navLinkTitleOne}>
+            <Button color='inherit' style={{ color: 'white' }}>
+              {navLinkDisplayOne}
+            </Button>
+          </NavLink>
+
+          <NavLink activeStyle={{ color: 'red' }} exact to={navLinkTitleTwo}>
+            <Button color='inherit' style={{ color: 'white' }}>
+              {navLinkDisplayTwo}
+            </Button>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Navbar;
